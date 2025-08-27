d1ef733861f05e347e973195806ea36d21059c3c
David Swift <david.swift@magnopus.com>
2025-08-27 15:08:10 -0700

    test: test some basic types of function

--- Tools/WrapperGenerator/tests/test_parser.py
+++ Tools/WrapperGenerator/tests/test_parser.py
@@ -78,3 +78,117 @@ class TestParser(unittest.TestCase):
         self.assertEqual(result.is_flags, False)
         self.assertEqual(result.is_nested_type, False)
         self.assertEqual(result.doc_comments, None)
+
+
+class TestParserParseFunction(unittest.TestCase):
+    def setUp(self):
+        self.parser = Parser.Parser(log_file=sys.stdout)
+        self.maxDiff = None
+
+    def __parse_function(self, function_str: str) -> MetadataTypes.FunctionMetadata:
+        wordreader = WordReader(function_str)
+        word = wordreader.next_word()  # Advance to first word
+        func = self.parser._Parser__parse_function("test.h", wordreader, word)
+        self.assertIsNone(wordreader.next_word())  # Ensure we consumed all words
+        return func
+    
+    def __parse_type(self, type_str: str) -> MetadataTypes.TypeMetadata:
+        wordreader = WordReader(type_str)
+        word = wordreader.next_word()
+        type, word = self.parser._Parser__parse_type(wordreader, word)
+        self.assertIsNone(word)  # Ensure we consumed all words
+        return type
+
+    def test_missing_semicolon_wtf(self):
+        """ Test that if you leave off the semicolon, parsing fails in an exciting fashion. """
+        with self.assertRaisesRegex(Parser.ParserError, ".*(WTF).*"):
+            result = self.__parse_function("void FunctionName()")
+
+    def test_parse_function_no_params(self):
+        """ Test parsing a function with no parameters. """
+        result = self.__parse_function(
+        """
+        void FunctionName();
+        """)
+
+        self.assertEqual(result.namespace, "")
+        self.assertEqual(result.name, "FunctionName")
+        self.assertEqual(result.return_type, None)
+        self.assertEqual(result.parameters, [])
+        self.assertEqual(result.is_static, False)
+        self.assertEqual(result.is_const, False)
+        self.assertEqual(result.doc_comments, None)
+
+    def test_parse_function_with_params(self):
+        """ Test parsing a function with parameters. """
+        result = self.__parse_function("int FunctionName(int param1);")
+
+        int_type = self.__parse_type("int")
+
+        arg1 = MetadataTypes.ParameterMetadata(
+            name="param1",
+            type=int_type,
+            is_out=False,
+            is_in_out=False,
+            is_last=True)
+
+        expected = MetadataTypes.FunctionMetadata(
+            header_file="test.h",
+            start_line=1,
+            end_line=1,
+            namespace="",
+            name="FunctionName",
+            return_type=int_type,
+            has_return=True,
+            has_parameters=True,
+            parameters=[arg1])
+
+        # We don't care about these fields for this test
+        expected.header_file = result.header_file
+
+        self.assertDictEqual(result.__dict__, expected.__dict__)
+
+    def test_parse_function_with_multiple_params(self):
+        """ Test parsing a function with multiple parameters. """
+        result = self.__parse_function("float FunctionName(int param1, const char* param2, double param3);")
+
+        int_type = self.__parse_type("int")
+        const_char_ptr_type = self.__parse_type("const char*")
+        double_type = self.__parse_type("double")
+
+        arg1 = MetadataTypes.ParameterMetadata(
+            name="param1",
+            type=int_type,
+            is_out=False,
+            is_in_out=False,
+            is_last=False)
+
+        arg2 = MetadataTypes.ParameterMetadata(
+            name="param2",
+            type=const_char_ptr_type,
+            is_out=False,
+            is_in_out=False,
+            is_last=False)
+
+        arg3 = MetadataTypes.ParameterMetadata(
+            name="param3",
+            type=double_type,
+            is_out=False,
+            is_in_out=False,
+            is_last=True)
+
+        expected = MetadataTypes.FunctionMetadata(
+            header_file="test.h",
+            start_line=1,
+            end_line=1,
+            namespace="",
+            name="FunctionName",
+            return_type=self.__parse_type("float"),
+            has_return=True,
+            has_parameters=True,
+            parameters=[arg1, arg2, arg3])
+
+        # We don't care about these fields for this test
+        expected.header_file = result.header_file
+
+        self.assertDictEqual(result.__dict__, expected.__dict__)
